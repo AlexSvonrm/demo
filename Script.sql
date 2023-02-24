f4e8d1640fbba2de68c4ef7b646f8fe901829625
AlexSvonrm <115774790+AlexSvonrm@users.noreply.github.com>
2023-02-24 20:03:12 +0300

    Add files via upload

--- Script.sql
+++ Script.sql
@@ -1,25 +1,21 @@
 CREATE TABLE IF NOT EXISTS Genre (
 	ID_Genre SERIAL PRIMARY KEY,
-	Name_Genre VARCHAR(80) NOT null
-);
+	Name_Genre VARCHAR(80) not null);
 
 CREATE TABLE IF NOT EXISTS Singer (
-	ID_Genre INTEGER PRIMARY KEY REFERENCES Genre(ID_Genre),
-	ID_Singer SERIAL NOT NULL,
-	Name_Singer VARCHAR(80) NOT NULL
-);
+	ID_Singer SERIAL primary key,
+	ID_Genre INTEGER not null references Genre(ID_Genre),
+	Name_Singer VARCHAR(80) not null);
 
 CREATE TABLE IF NOT EXISTS Album (
-	ID_Singer INTEGER PRIMARY KEY REFERENCES Singer(ID_Singer),
-	ID_Album SERIAL NOT NULL,
+	ID_Album SERIAL PRIMARY KEY,
+	ID_Singer INTEGER NOT NULL REFERENCES Singer(ID_Singer),
 	Name_Album VARCHAR(80) NOT NULL,
-	Yearset INT(4) NOT NULL
-);
+	Yearset INTEGER NOT NULL);
 
 CREATE TABLE IF NOT EXISTS Track (
-	ID_Album INTEGER PRIMARY KEY REFERENCES Album(ID_Album),
-	ID_Track SERIAL NOT NULL,
+	ID_Track SERIAL PRIMARY KEY,
+	ID_Album INTEGER NOT NULL REFERENCES Album(ID_Album),
 	Name_Track VARCHAR(80) NOT NULL,
-	Duration FLOAT
-);
+	Duration FLOAT);
 
