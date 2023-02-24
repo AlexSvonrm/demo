bb8afa01ba6eca6d54dd63c1ebbab50b112cb551
AlexSvonrm <115774790+AlexSvonrm@users.noreply.github.com>
2023-02-25 02:11:37 +0300

    Add files via upload

--- Script.sql
+++ Script.sql
@@ -1,21 +1,41 @@
-CREATE TABLE IF NOT EXISTS Genre (
-	ID_Genre SERIAL PRIMARY KEY,
-	Name_Genre VARCHAR(80) not null);
+create table if not exists artist_list(
+	id serial primary key,
+	Name varchar(255) not null,
+	Alias varchar(80) not null);
 
-CREATE TABLE IF NOT EXISTS Singer (
-	ID_Singer SERIAL primary key,
-	ID_Genre INTEGER not null references Genre(ID_Genre),
-	Name_Singer VARCHAR(80) not null);
+create table if not exists Genre_list(
+	id serial primary key, 
+	Genre_Name  varchar(80) not null);	
 
-CREATE TABLE IF NOT EXISTS Album (
-	ID_Album SERIAL PRIMARY KEY,
-	ID_Singer INTEGER NOT NULL REFERENCES Singer(ID_Singer),
-	Name_Album VARCHAR(80) NOT NULL,
-	Yearset INTEGER NOT NULL);
+create table if not exists artist_in_genre(
+	id serial primary key,
+	artist_id integer not null references artist_list(id),
+	genre_id integer not null references Genre_List(id));
 
-CREATE TABLE IF NOT EXISTS Track (
-	ID_Track SERIAL PRIMARY KEY,
-	ID_Album INTEGER NOT NULL REFERENCES Album(ID_Album),
-	Name_Track VARCHAR(80) NOT NULL,
-	Duration FLOAT);
+create table if not exists album_List (
+	id serial primary key,
+	album_name varchar(80) not null,
+	release_date varchar(80) not null);				
 
+create table if not exists artist_album(
+	id  serial primary key,
+	artist_id integer not null references artist_list(id),
+	album_id integer not null references album_List(id));
+	
+create table if not exists Track_List (
+	id serial primary key,
+	album_id integer references album_List(id),
+	track_name varchar(80) not null,
+	track_time varchar(80) not null);
+		
+create table if not exists collection_List(
+	id serial primary key,
+	collection_name varchar(60) not null,
+	release_date date not null);
+							
+create table if not exists collection_album_track(
+	id serial primary key,
+	collection_id integer not null references collection_List(id),
+	album_id integer not null references album_List(id),
+	track_id integer not null references Track_List(id)
+);
